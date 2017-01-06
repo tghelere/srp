a3ebb5bdf7ee563860fd9d3a0044ad3315e77e5b
tghelere <ghelere@outlook.com>
2017-01-06 21:09:07 -0200

    instalação do fancybox

--- app/front/js/main.js
+++ app/front/js/main.js
@@ -79,4 +79,28 @@ $(document).ready(function () {
     $('.mapa iframe').css("pointer-events", "none");
   });
 
+//fancybox
+  $('.fancy').fancybox({
+    openEffect  : 'none',
+    closeEffect : 'none',
+
+    prevEffect : 'none',
+    nextEffect : 'none',
+
+    closeBtn  : false,
+
+    helpers : {
+      title : {
+        type : 'inside'
+      },
+      buttons	: {}
+    },
+
+    afterLoad : function() {
+      this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+    }
+  });
+
+// 
+
 });
