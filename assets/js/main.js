e29862852c95f88c1494a2df4517b620ccb49634
tghelere <ghelere@outlook.com>
2017-12-05 21:00:49 -0200

    correção das notificações para envio de emails - atualizado plugin noty

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,46 @@
 $(document).ready(function () {
 
-//nanobar
-  var colorbar = new Nanobar({target: document.getElementById('color')})
-  colorbar.go(100)
+//nanobar - barra de carregamento da pagina
+      var colorbar = new Nanobar({target: document.getElementById('color')})
+      colorbar.go(100)
 
-//Alertas
-  // var alertas = $("#alertas")
-  // $.ajax({
-  //     type: "POST",
-  //     url: "/alert/",
-  //     dataType: "json",
-  //     success: function (data) {
-  //       if(data != null){
-  //         var alert = noty({text: data.msg, type: data.type, theme: 'metroui', layout: 'top', timeout: 10000, closeWith: ['click']})
-  //         $("#alertas").html('<script type="text/javascript">'+alert+'</script>')
-  //       }
-  //       // noty({type:'alert'})
-  //       // noty({type:'information'})
-  //       // noty({type:'error'})
-  //       // noty({type:'warning'})
-  //       // noty({type:'success'})
-  //     }
-  // })
+// Alertas
+      var alertas = $("#alertas")
+      $.ajax({
+          type: "POST",
+          url: "/alert/",
+          dataType: "json",
+          success: function (data) {
+            if(data != null){
+              new Noty({
+                text: data.msg,
+                type: data.type,
+                layout: 'top',
+                timeout: 5000,
+              }).show();
+            }
+          }
+      })
 
-//menu
-  $('.menu-open').click(function() {
-    $('.open-menu').toggleClass('hidden')
-    $('#navbar-close').toggleClass('hidden')
-  })
-
+//menu mobile
+      $('.menu-open').click(function() {
+        $('.open-menu').toggleClass('hidden')
+        $('#navbar-close').toggleClass('hidden')
+      })
 
 // modal
-  $('.modal-img').on('click', function () {
-    $('.imagepreview').attr('src', $(this).find('img').attr('src'))
-  })    
+      $('.modal-img').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'))
+      })    
 
-//Scroll Menu   
-   $(window).on('scroll', () => {
-       if ($(window).scrollTop() > $('.carousel').height()) {
-           $('.main-nav').addClass('navbar-fixed-top')
-       } else {
-           $('.main-nav').removeClass('navbar-fixed-top')
-       }
-   })
+//Scroll Menu desktop 
+      $(window).on('scroll', () => {
+          if ($(window).scrollTop() > $('.carousel').height()) {
+              $('.main-nav').addClass('navbar-fixed-top')
+          } else {
+              $('.main-nav').removeClass('navbar-fixed-top')
+          }
+      })
 
 // mascara do form contato
   var maskBehavior = function (val) {
@@ -54,35 +52,7 @@ $(document).ready(function () {
   }
 
   $('.fone').mask(maskBehavior, options)
-
-// scroll do mapa na pagina contato
-  $('.mapa').click(function () {
-    $('.mapa iframe').css("pointer-events", "auto")
-  })
-
-  $( ".mapa" ).mouseleave(function() {
-    $('.mapa iframe').css("pointer-events", "none")
-  })
-
-//fancybox
-  $('.fancy').fancybox({
-    openEffect  : 'none',
-    closeEffect : 'none',
-
-    prevEffect : 'none',
-    nextEffect : 'none',
-
-    closeBtn  : false,
-
-    helpers : {
-      title : {
-        type : 'inside'
-      },
-      buttons	: {}
-    },
-
-    afterLoad : function() {
-      this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '')
-    }
-  })
 })
+
+
+
